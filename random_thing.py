ed00b087751fd8ada6900f4c76895fec6053f0d7
t95229ld <louise.dennis@manchester.ac.uk>
2026-01-08 14:35:46 +0000

    Updating list.

--- random_thing.py
+++ random_thing.py
@@ -1,6 +1,6 @@
 from random import choice
 
-list = ["Who Game Programming", "Waistcoat Making", "Planning Offa's Dyke Walk", "Listening to Music", "Clear out Email", "Read some Dutch", "Learn Dutch", "Bingo Comm Programming", "Practice Music", "Find/Play Game", "Organise Computer", "Recipes from the Book", "Pokemon Crochet", "Lego", "Catalogue and Count Dad's Slides"]
+list = ["Who Game Programming", "Waistcoat Making", "Planning Offa's Dyke Walk", "Listening to Music", "Clear out Email", "Read some Dutch", "Learn Dutch", "Bingo Comm Programming", "Practice Music", "Find/Play Game", "Organise Computer", "Lego", "Costume Bracket", "Doctor Who Icons"]
         
 print(str(choice(list)))
         
